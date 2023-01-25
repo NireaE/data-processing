b800ecb9ed9995dcd9c1a90bda6decf0fe11a37c
Elias <eliasgebru0@gmail.com>
2023-01-24 19:42:34 -0600

    Add title case for the form

--- js/data-processing.js
+++ js/data-processing.js
@@ -2,18 +2,61 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/forEach
 const queryString = window.location.search;
 
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+}
+
 if(queryString.length > 0){
     
     const urlParams = new URLSearchParams(queryString);
     
     let myData = "";
-
+    let myCart = "";
+    let myTotal = 0;   //will store 
+     
+    myCart += "<h3>Cart Contents</h3>";
     // Log the values
     urlParams.forEach(function(value, key) {
-    key = key.split("_").join(" ");
-    // console.log(value, key);
-    myData += `<p>${key}: ${value}</p>`;
+       
+        if(key == "Cart"){//process cart
+            
+            switch(value){
+                case "Widget":
+                    myCart += "Widget: $3.99<br>";
+                    myTotal +=3.99;
+                break;
+                case "Sprocket":
+                    myCart += "Sprocket: $5.99<br>";
+                    myTotal +=5.99;
+                break;
+                case "Thingy":
+                    myCart += " Thingy: $1.99<br>";
+                    myTotal +=1.99;
+                break;
+
+            }
+        
+        }else{//process shipping 
+                
+            key = key.split("_").join(" ");
+            if(key == "First Name" || key == "Last Name" || key == "City"){
+                value = titleCase(value);
+            }
+            myData += `<p>${key}: ${value}</p>`;
+            
+        }
+        
     });
+
+    myCart += "Total:" + myTotal + '<br>';
+
+    // we want Cart  data to go first
+    myData = myCart + myData;
+ 
     myData += '<p><a href="index.html">CLEAR</a></p>';
     document.getElementById("output").innerHTML = myData;
 }
